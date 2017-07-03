55b24b9f4150cba4e13033edcad384b6d09d100b
rodrigodzf <rodrigodzf@gmail.com>
2017-07-03 17:26:51 +0200

    added writeing files

--- Tone_generator.py
+++ Tone_generator.py
@@ -3,22 +3,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.io.wavfile import write
 
-"Dry Signal generator"
-Fs = 44100
-drySignal = list()
-for freq in range(40, 22000, 5):
-    t = np.linspace(0, 0.075, Fs*0.075, endpoint=False)
-    drySignal.append(signal.square(2 * np.pi * freq * t))
-    # plt.plot(t, signal.square(2 * np.pi * freq * t))
-    # plt.ylim(-2, 2)
-    # plt.show()
-# print drySignal
+def generate_signal(foldername):
+    Fs = 16000
+    for freq in range(40, 5000, 500):
+        samples = 4096
+        t = np.linspace(0, 1 / Fs * samples, samples)  # generate samples
+        sig = np.sin(2 * np.pi * freq * t)
+        name = foldername + "p" + str(0) + "_" + str(freq) + ".wav"
+        write(name, 16000, sig)
 
-"Filter Design"
-filtSignal = list()
-for wn in range(20, 12000, 10):
-    wn = float(wn) / (Fs/2)
-    b, a = signal.butter(2, wn, 'lowpass')
-    filtSignal.append(signal.lfilter(b, a, drySignal))
+# "Filter Design"
+# filtSignal = list()
+# for wn in range(20, 12000, 10):
+#     wn = float(wn) / (Fs / 2)
+#     b, a = signal.butter(2, wn, 'lowpass')
+#     filtSignal.append(signal.lfilter(b, a, drySignal))
 
-
+generate_signal('corpus/')
